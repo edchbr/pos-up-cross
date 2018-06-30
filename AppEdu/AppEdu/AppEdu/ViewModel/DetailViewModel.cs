e169a8e2ab8a28320a73e1c141f2fdd36eeabe5c
Eduardo <edchbr@gmail.com>
2018-06-30 09:08:05 -0300

    primeira aula

---
+++ AppEdu/AppEdu/AppEdu/ViewModel/DetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using AppEdu.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppEdu.ViewModel
+{
+    public class DetailViewModel : ViewModelBase
+    {
+        public DetailViewModel() : base("")
+        {
+        }
+    }
+}
---
+++ AppEdu/AppEdu/AppEdu/ViewModel/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using AppEdu.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppEdu.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel() : base("Awesome Series")
+        {
+        }
+    }
+}
